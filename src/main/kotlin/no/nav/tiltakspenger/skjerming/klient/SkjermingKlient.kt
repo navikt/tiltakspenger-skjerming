a942e3c7059784862748a7fe7133d8838c43430b
Hågen Hasle <haagenhasle@gmail.com>
2022-09-28 23:47:42 +0200

    Prøver med bearerAuth isf auth pluginen

--- src/main/kotlin/no/nav/tiltakspenger/skjerming/klient/SkjermingKlient.kt
+++ src/main/kotlin/no/nav/tiltakspenger/skjerming/klient/SkjermingKlient.kt
@@ -1,20 +1,12 @@
 package no.nav.tiltakspenger.skjerming.klient
 
 import com.fasterxml.jackson.databind.ObjectMapper
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.engine.HttpClientEngine
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BearerTokens
-import io.ktor.client.plugins.auth.providers.bearer
-import io.ktor.client.request.accept
-import io.ktor.client.request.header
-import io.ktor.client.request.preparePost
-import io.ktor.client.request.setBody
-import io.ktor.http.ContentType
-import io.ktor.http.HttpStatusCode
-import io.ktor.http.contentType
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 import no.nav.tiltakspenger.skjerming.Configuration
 import no.nav.tiltakspenger.skjerming.defaultHttpClient
 import no.nav.tiltakspenger.skjerming.defaultObjectMapper
@@ -27,20 +19,7 @@ class SkjermingKlient(
     private val httpClient: HttpClient = defaultHttpClient(
         objectMapper = objectMapper,
         engine = engine
-    ) {
-        install(Auth) {
-            bearer {
-                loadTokens {
-                    BearerTokens(
-                        accessToken = getToken(),
-                        // Refresh token are used in refreshToken method if client gets 401
-                        // Should't need this if token expiry is checked first
-                        refreshToken = emptyRefreshToken,
-                    )
-                }
-            }
-        }
-    }
+    ) {}
 ) {
     companion object {
         const val emptyRefreshToken = ""
@@ -51,6 +30,7 @@ class SkjermingKlient(
     suspend fun erSkjermetPerson(fødselsnummer: String, behovId: String): Boolean {
         val httpResponse = httpClient.preparePost("${skjermingConfig.baseUrl}/skjermet") {
             header(navCallIdHeader, behovId)
+            bearerAuth(getToken())
             accept(ContentType.Application.Json)
             contentType(ContentType.Application.Json)
             setBody(SkjermetDataRequestDTO(fødselsnummer))
